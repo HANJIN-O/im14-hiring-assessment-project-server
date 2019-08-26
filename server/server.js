a4175d198e2ba14374ea12f872e55e4f72b80df6
HAN-O <zeros0623@gmail.com>
2019-08-27 01:00:47 +0900

    init

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+app.get
+
+app.listen(5000, ()=> {
+  console.log('server listen on 5000');
+})
