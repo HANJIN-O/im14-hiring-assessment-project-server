66ed1efcbd2e9cebe478ddcffcfa6d9f07c083af
HAN-O <zeros0623@gmail.com>
2019-08-29 03:39:40 +0900

    init

--- server/App.js
+++ server/App.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const Route = require('./router')
 
 const app = express();
 app.use(cors());
 
-app.get
+app.get('/', Route);
 
 app.listen(5000, ()=> {
   console.log('server listen on 5000');
